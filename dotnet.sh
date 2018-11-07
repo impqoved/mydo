20455f656e7d99d1e1fa7b1b06131a869082bd9a
impqoved <253881771@qq.com>
2018-11-07 13:37:11 +0800

    Update dotnet.sh

--- dotnet.sh
+++ dotnet.sh
@@ -1,3 +1,4 @@
+wget https://github.com/JustArchiNET/ArchiSteamFarm/releases/download/3.4.0.6/ASF-generic.zip
 wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
 dpkg -i packages-microsoft-prod.deb
 apt-get install apt-transport-https
